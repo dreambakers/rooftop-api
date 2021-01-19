10f0c9f80e7d61461b6b0b0be78ade19715333f3
Mahad Amir <mamir3012@gmail.com>
2021-01-19 20:50:30 +0500

    Expose x-auth header

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const routes = require('./routes/routes');
 const winston = require('./config/winston');
 
 const connectDB = require('./config/db');
-
+const port = process.env.PORT || 9000;
 const app = express();
 
 // Logging Middleware
@@ -22,7 +22,14 @@ app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
-const port = process.env.PORT || 9000;
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'x-auth,Content-Type');
+  res.setHeader('Access-Control-Expose-Headers', 'x-auth,Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.get('/', (req, res) => { res.send('Rooftop API is running.') });
 
